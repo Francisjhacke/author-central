516bc027e841f85a65b7d53dac47faad3d871520
Francis <francisjhackenberger@gmail.com>
2018-07-24 20:24:21 -0400

    removing server folder

--- server/index.ts
+++
@@ -1,7 +0,0 @@
-import app from './src';
-import config from './src/config';
-import logger from './src/util/logger';
-
-app.listen(config.port, () => {
-    logger.log(`server started at port ${config.port}`);
-});
--- server/src/api/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { sampleRouter } from './sample/sample.router';
-
-let router = Router();
-router.use('/sample', sampleRouter);
-
-export let apiRouter = router;
-
