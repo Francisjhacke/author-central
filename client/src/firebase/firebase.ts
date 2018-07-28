f45080aa2d849bce6cf57465fbef6b7d6ac1e01a
Francis Hackenberger <francisjhackenberger@gmail.com>
2018-07-28 00:09:34 -0400

    Update firebase.ts

--- client/src/firebase/firebase.ts
+++ client/src/firebase/firebase.ts
@@ -2,14 +2,6 @@ import * as firebase from "firebase";
 import "firebase/auth";
 import "firebase/database";
 
-const config = {
-  apiKey: "AIzaSyBC7EixbPEh_IGpJUa95wsBAh5QbYcp0Uk",
-  authDomain: "author-central-905.firebaseapp.com",
-  databaseURL: "https://author-central-905.firebaseio.com",
-  messagingSenderId: "310131066353",
-  projectId: "author-central-905",
-  storageBucket: "author-central-905.appspot.com"
-};
 
 
 if (!firebase.apps.length) {
