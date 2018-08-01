26d4426dbea55d307907c16d83c7bb5f8c28531b
Francis <francisjhackenberger@gmail.com>
2018-08-01 19:50:59 -0400

    Fixed structure for firebase config

--- client/src/firebase/firebase.ts
+++ client/src/firebase/firebase.ts
@@ -2,7 +2,14 @@ import * as firebase from "firebase";
 import "firebase/auth";
 import "firebase/database";
 
-
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
