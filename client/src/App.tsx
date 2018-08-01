df53818a4891305c287bf7387481e9b5885d3b2a
Francis <francisjhackenberger@gmail.com>
2018-08-01 19:19:20 -0400

    Moved main App component to the client src root

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import * as routes from "../constants/routes";
-import { firebase } from "../firebase";
-import { withAuthentication } from "../firebase/withAuthentication";
-import { Account } from "../pages/Account";
-import { Home } from "../pages/Home";
-import { Landing } from "../pages/Landing";
-import { PasswordForget } from "../pages/PasswordForget";
-import { SignIn } from "../pages/SignIn";
-import { SignUp } from "../pages/SignUp";
-import { Navigation } from "./Navigation";
+import * as routes from "./constants/routes";
+import { firebase } from "./firebase";
+import { withAuthentication } from "./firebase/withAuthentication";
+import { Account } from "./pages/Account";
+import { Home } from "./pages/Home";
+import { Landing } from "./pages/Landing";
+import { PasswordForget } from "./pages/PasswordForget";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import { Navigation } from "./components/Navigation";
 
 class AppComponent extends React.Component {
   constructor(props: any) {
